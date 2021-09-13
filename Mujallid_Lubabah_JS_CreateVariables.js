296251d71f85a49c980ad18829fe63971c29b4de
Lubabah-mujallid <90623112+Lubabah-mujallid@users.noreply.github.com>
2021-09-13 16:03:35 +0300

    Update Mujallid_Lubabah_JS_CreateVariables.js

--- Mujallid_Lubabah_JS_CreateVariables.js
+++ Mujallid_Lubabah_JS_CreateVariables.js
@@ -1,15 +1,3 @@
-/*The Story: A local theme park has a ride where 
-the rider must be at least 42 inches tall to ride and is over the age of 10.
- They've hired the development agency you work at to build an app
-  where once it measures a rider's height and age, can display whether the rider
-   is tall enough and old enough to ride the rollercoaster.
-    You've been tasked with building the variables for this app.
-
-
-
-Step 3: In your same VS Code file, use comments to explain 
-what the variables must be set to in order for the rider to ride. 
-*/
 
 var height = 0;     //should be at least 42 inches
 var age = 0;        //should be over 10 years
